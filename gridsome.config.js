8e04ce648516a3111c538eb2378d9767b5805cf5
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-01 14:27:47 -0700

    changed site data

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'DerekGilbert.io',
+  siteDescription: 'An expression of of code, politics & things I think about. Read at your own parrel',
 
   plugins: [
     {
