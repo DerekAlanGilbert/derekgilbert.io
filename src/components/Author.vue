139905594abbd4fd5921e1be704b2e755992d8cf
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-01 23:18:20 -0700

    Changed author

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,12 +8,12 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			An expression of Code, Politics & things I think about. Read at your own parrel
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//twitter.com/derekalangilbert">Follow on Twitter</a>
+			<a href="//github.com/gridsome/derekalangilbert">GitHub</a>
 		</p>
 
 	</div>
