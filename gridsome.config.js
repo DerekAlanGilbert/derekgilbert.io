f796b543ff95370529833e156dad051986a676cf
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-03 15:49:27 -0700

    moved folder configuration

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,7 +14,7 @@ module.exports = {
       use: '@gridsome/source-filesystem',
       options: {
         typeName: 'Post',
-        path: 'content/posts/*.md',
+        path: 'blog/*.md',
         route: '/:slug',
         refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
