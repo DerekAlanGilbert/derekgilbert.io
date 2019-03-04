ad3ffce3b5ba95b2851d9cb443cd4f052ece3854
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-03 20:55:44 -0700

    contact page

---
+++ src/pages/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout :show-logo="false">
+    <!-- Author intro -->
+    <Author :show-title="true"  :show-links="true"/>
+    
+    <!-- Contact Form -->
+    <ContactForm/>
+
+  </Layout> 
+</template>
+
+
+<script>
+import Author from '~/components/Author.vue'
+import ContactForm from '~/components/ContactForm.vue'
+
+export default {
+  components: {
+    Author,
+    ContactForm
+  },
+
+}
+</script>
+
