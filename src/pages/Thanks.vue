8b3fb080aeddf8fa465b1e628af7036ce903d05a
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-03 20:55:48 -0700

    added trhanks page

---
+++ src/pages/Thanks.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout :show-logo="false">
+    <!-- Author intro -->
+    <Author :show-title="true"  :show-links="true"/>
+    
+    <!-- Return to Home -->
+      <div class="content-box text-center">
+        <h1> Thanks for contacting me, we'll be in touch </h1>
+        <g-link to="/">Return Home</g-link>
+      </div>
+  </Layout> 
+</template>
+
+
+<script>
+import Author from '~/components/Author.vue'
+
+export default {
+  components: {
+    Author,
+  },
+
+}
+</script>
+ <style lang="scss" scoped>
+h1 {
+  color: var(--body-color);
+}
+</style>
+
+
+
